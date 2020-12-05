af64cccf69e45cea6156921a96652974194e0900
rachidbt <rachidglobalia@gmail.com>
2020-12-05 11:59:55 +0100

    entrega

---
+++ Functions_PHP_exercises_1_to_learn.php
@@ -0,0 +1,31 @@
+<?php
+function suma($valor1, $valor2)
+{
+    return ($valor1 + $valor2);
+}
+echo suma(4, 6);
+?>
+
+<?php
+function subtraction($valor1, $valor2)
+{
+    return ($valor1 - $valor2);
+}
+echo subtraction(4, 6);
+?>
+
+<?php
+function multplication($valor1, $valor2)
+{
+    return ($valor1 * $valor2);
+}
+echo multplication(4, 6);
+?>
+
+<?php
+function division($valor1, $valor2)
+{
+    return ($valor1 / $valor2);
+}
+echo division(4, 6);
+?>
